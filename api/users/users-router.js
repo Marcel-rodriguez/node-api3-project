b3e8e611c2544640bd531885507e29e164800081
marcel <marcel_rodrgez@yahoo.com>
2022-02-10 02:21:22 -0500

    Fixed posting a new post

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -83,7 +83,7 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
 
     postModel.insert(post)
     .then(newPost => {
-      res.status(210).json(post)
+      res.status(210).json(newPost)
     }).catch(next)
 
 });
